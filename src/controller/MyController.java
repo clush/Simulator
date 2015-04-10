922a7185813a338eaf2c1f0904ff13ff07cd5aa2
Thomas <tcoenen@gmx.net>
2015-04-10 10:16:53 +0200

    Import auskommentiert
    Quellen auskommentiert

--- src/controller/MyController.java
+++ src/controller/MyController.java
@@ -17,10 +17,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.text.TextAlignment;
-import javafx.scene.text.TextFlow;
-import javafx.stage.DirectoryChooser;
+//import javafx.scene.control.TextField;
+//import javafx.scene.text.TextAlignment;
+//import javafx.scene.text.TextFlow;
+//import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Window;
 
